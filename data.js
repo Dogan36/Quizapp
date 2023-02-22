aa726023ccf8a7b54f10b92fcd623380d308901a
Dogan Celik <dogancelik86@gmail.com>
2023-02-22 16:00:52 +0100

    first commit

---
+++ data.js
@@ -0,0 +1,371 @@
+let htmlQuestions = [
+    {
+        "frage": "Was steht für HTML?",
+        "antwort 1": "Hyper Text Markup Language",
+        "antwort 2": "Hyperlinks and Text Markup Language",
+        "antwort 3": "Home Tool Markup Language",
+        "antwort 4": "Keine der oben genannten Antworten",
+        "richtige antwort": "antwort 1"
+    },
+    {
+        "frage": "Welches Element wird verwendet, um eine Überschrift der ersten Ebene zu definieren?",
+        "antwort 1": "<h1>",
+        "antwort 2": "<heading>",
+        "antwort 3": "<h6>",
+        "antwort 4": "<header>",
+        "richtige antwort": "antwort 1"
+    },
+    {
+        "frage": "Welches Element wird verwendet, um einen Absatz zu definieren?",
+        "antwort 1": "<p>",
+        "antwort 2": "<paragraph>",
+        "antwort 3": "<para>",
+        "antwort 4": "<section>",
+        "richtige antwort": "antwort 1"
+    },
+    {
+        "frage": "Welches Element wird verwendet, um einen Link zu definieren?",
+        "antwort 1": "<link>",
+        "antwort 2": "<a>",
+        "antwort 3": "<href>",
+        "antwort 4": "<url>",
+        "richtige antwort": "antwort 2"
+    },
+    {
+        "frage": "In welchem Element befindet sich der gesamte Inhalt einer Webseite?",
+        "antwort 1": "<body>",
+        "antwort 2": "<head>",
+        "antwort 3": "<html>",
+        "antwort 4": "<meta>",
+        "richtige antwort": "antwort 1"
+    },
+    {
+        "frage": "Welches Element wird verwendet, um eine nummerierte Liste zu definieren?",
+        "antwort 1": "<ol>",
+        "antwort 2": "<list>",
+        "antwort 3": "<ul>",
+        "antwort 4": "<dl>",
+        "richtige antwort": "antwort 1"
+    },
+    {
+        "frage": "Welches Element wird verwendet, um eine nicht nummerierte Liste zu definieren?",
+        "antwort 1": "<ul>",
+        "antwort 2": "<ol>",
+        "antwort 3": "<li>",
+        "antwort 4": "<ul>",
+        "richtige antwort": "antwort 1"
+    },
+    {
+        "frage": "Welches Element wird verwendet, um ein Bild einzufügen?",
+        "antwort 1": "<img>",
+        "antwort 2": "<picture>",
+        "antwort 3": "<image>",
+        "antwort 4": "<photo>",
+        "richtige antwort": "antwort 1"
+    },
+    {
+        "frage": "Welches Element wird verwendet, um einen Absatz zu erzwingen?",
+        "antwort 1": "<br>",
+        "antwort 2": "<lb>",
+        "antwort 3": "<p>",
+        "antwort 4": "<cr>",
+        "richtige antwort": "antwort 1"
+    },
+    {
+        "frage": "Welches Element wird verwendet, um eine horizontale Linie zu definieren?",
+        "antwort 1": "<hr>",
+        "antwort 2": "<line>",
+        "antwort 3": "<br>",
+        "antwort 4": "<tr>",
+        "richtige antwort": "antwort 1"
+    }
+]
+
+let cssQuestions = [
+    {
+        "frage": "Was steht für CSS?",
+        "antwort 1": "Cascading Style Sheets",
+        "antwort 2": "Creative Style Sheets",
+        "antwort 3": "Computer Style Sheets",
+        "antwort 4": "None of the above",
+        "richtige antwort": "antwort 1"
+    },
+    {
+        "frage": "Welches Attribut wird verwendet, um den Hintergrundfarbeigenschaftswert eines Elements festzulegen?",
+        "antwort 1": "background-color",
+        "antwort 2": "color",
+        "antwort 3": "background-image",
+        "antwort 4": "background-style",
+        "richtige antwort": "antwort 1"
+    },
+    {
+        "frage": "Welche Einheit wird verwendet, um die Breite eines Elements in CSS zu definieren?",
+        "antwort 1": "px",
+        "antwort 2": "em",
+        "antwort 3": "%",
+        "antwort 4": "Alle der oben genannten Antworten",
+        "richtige antwort": "antwort 4"
+    },
+    {
+        "frage": "Welches Attribut wird verwendet, um den Textfarbeigenschaftswert eines Elements festzulegen?",
+        "antwort 1": "color",
+        "antwort 2": "text-color",
+        "antwort 3": "font-color",
+        "antwort 4": "None of the above",
+        "richtige antwort": "antwort 1"
+    },
+    {
+        "frage": "Welches Attribut wird verwendet, um den Abstand zwischen den Zeilen eines Textes zu definieren?",
+        "antwort 1": "line-height",
+        "antwort 2": "font-size",
+        "antwort 3": "letter-spacing",
+        "antwort 4": "text-spacing",
+        "richtige antwort": "antwort 1"
+    },
+    {
+        "frage": "Welches Attribut wird verwendet, um die Schriftart eines Elements festzulegen?",
+        "antwort 1": "font-family",
+        "antwort 2": "font-type",
+        "antwort 3": "font-style",
+        "antwort 4": "None of the above",
+        "richtige antwort": "antwort 1"
+    },
+    {
+        "frage": "Welches Attribut wird verwendet, um den Abstand um ein Element herum festzulegen?",
+        "antwort 1": "padding",
+        "antwort 2": "margin",
+        "antwort 3": "border",
+        "antwort 4": "outline",
+        "richtige antwort": "antwort 2"
+    },
+    {
+        "frage": "Welches Attribut wird verwendet, um den Text in Großbuchstaben umzuwandeln?",
+        "antwort 1": "text-transform: uppercase",
+        "antwort 2": "text-decoration: uppercase",
+        "antwort 3": "text-style: uppercase",
+        "antwort 4": "None of the above",
+        "richtige antwort": "antwort 1"
+    },
+    {
+        "frage": "Welches Attribut wird verwendet, um die Ausrichtung eines Elements zu definieren?",
+        "antwort 1": "text-align",
+        "antwort 2": "align",
+        "antwort 3": "position",
+        "antwort 4": "None of the above",
+        "richtige antwort": "antwort 1"
+    },
+    {
+        "frage": "Welches Attribut wird verwendet, um die Transparenz eines Elements festzulegen?",
+        "antwort 1": "opacity",
+        "antwort 2": "transparent",
+        "antwort 3": "visibility",
+        "antwort 4": "filter",
+        "richtige antwort": "antwort 1"
+    }
+]
+
+let jsQuestions = [
+    {
+        "frage": "Welches Schlüsselwort wird verwendet, um eine Variable zu deklarieren?",
+        "antwort 1": "var",
+        "antwort 2": "let",
+        "antwort 3": "const",
+        "antwort 4": "define",
+        "richtige antwort": "antwort 1"
+    },
+    {
+        "frage": "Was ist das Ergebnis von 3 + '3' in JavaScript?",
+        "antwort 1": "6",
+        "antwort 2": "33",
+        "antwort 3": "9",
+        "antwort 4": "undefined",
+        "richtige antwort": "antwort 2"
+    },
+    {
+        "frage": "Welches Schlüsselwort wird verwendet, um eine Funktion in JavaScript zu definieren?",
+        "antwort 1": "function",
+        "antwort 2": "def",
+        "antwort 3": "func",
+        "antwort 4": "define",
+        "richtige antwort": "antwort 1"
+    },
+    {
+        "frage": "Was ist der Unterschied zwischen '==' und '===' in JavaScript?",
+        "antwort 1": "'==' vergleicht den Wert, '===' vergleicht den Wert und den Typ",
+        "antwort 2": "'===' vergleicht den Wert, '==' vergleicht den Wert und den Typ",
+        "antwort 3": "'==' und '===' sind dasselbe",
+        "antwort 4": "Keine der obigen Antworten ist korrekt",
+        "richtige antwort": "antwort 1"
+    },
+    {
+        "frage": "Welches Schlüsselwort wird verwendet, um eine Bedingung in JavaScript zu definieren?",
+        "antwort 1": "if",
+        "antwort 2": "when",
+        "antwort 3": "condition",
+        "antwort 4": "choose",
+        "richtige antwort": "antwort 1"
+    },
+    {
+        "frage": "Welches Schlüsselwort wird verwendet, um eine Schleife in JavaScript zu definieren?",
+        "antwort 1": "loop",
+        "antwort 2": "repeat",
+        "antwort 3": "while",
+        "antwort 4": "for",
+        "richtige antwort": "antwort 4"
+    },
+    {
+        "frage": "Was ist das Ergebnis von 2 + 2 * 2 in JavaScript?",
+        "antwort 1": "6",
+        "antwort 2": "8",
+        "antwort 3": "4",
+        "antwort 4": "None of the above",
+        "richtige antwort": "antwort 2"
+    },
+    {
+        "frage": "Welches Schlüsselwort wird verwendet, um eine Ausnahmebehandlung in JavaScript zu definieren?",
+        "antwort 1": "exception",
+        "antwort 2": "try",
+        "antwort 3": "catch",
+        "antwort 4": "error",
+        "richtige antwort": "antwort 2"
+    },
+    {
+        "frage": "Welches Objekt wird verwendet, um die Ausgabe in die Konsole zu schreiben?",
+        "antwort 1": "console",
+        "antwort 2": "window",
+        "antwort 3": "document",
+        "antwort 4": "log",
+        "richtige antwort": "antwort 1"
+    },
+    {
+        "frage": "Was ist das Ergebnis von '5' - '3' in JavaScript?",
+        "antwort 1": "2",
+        "antwort 2": "8",
+        "antwort 3": "53",
+        "antwort 4": "NaN",
+        "richtige antwort": "antwort 1"
+    },
+    {
+        "frage": "Welches Objekt wird verwendet, um eine zufällige Zahl in JavaScript zu generieren?",
+        "antwort 1": "Math.random()",
+        "antwort 2": "random()",
+        "antwort 3": "Math.random",
+        "antwort 4": "random.Math()",
+        "richtige antwort": "antwort 1"
+    },
+    {
+        "frage": "Was ist das Ergebnis von 'hello'.toUpperCase() in JavaScript?",
+        "antwort 1": "'Hello'",
+        "antwort 2": "'hello'",
+        "antwort 3": "'HELLO'",
+        "antwort 4": "undefined",
+        "richtige antwort": "antwort 3"
+    },
+    {
+        "frage": "Welches Schlüsselwort wird verwendet, um eine Klasse in JavaScript zu definieren?",
+        "antwort 1": "class",
+        "antwort 2": "define",
+        "antwort 3": "klass",
+        "antwort 4": "type",
+        "richtige antwort": "antwort 1"
+    },
+    {
+        "frage": "Was ist das Ergebnis von typeof undefined in JavaScript?",
+        "antwort 1": "'null'",
+        "antwort 2": "'undefined'",
+        "antwort 3": "'object'",
+        "antwort 4": "'string'",
+        "richtige antwort": "antwort 2"
+    },
+    {
+        "frage": "Was ist das Ergebnis von [].length in JavaScript?",
+        "antwort 1": "0",
+        "antwort 2": "1",
+        "antwort 3": "undefined",
+        "antwort 4": "TypeError",
+        "richtige antwort": "antwort 1"
+    }
+]
+
+let nodeQuestions = [
+    {
+        "frage": "Was ist Node.js?",
+        "antwort 1": "Eine serverseitige JavaScript-Plattform",
+        "antwort 2": "Ein webbasierter JavaScript-Editor",
+        "antwort 3": "Ein JavaScript-Framework",
+        "antwort 4": "Ein JavaScript-Compiler",
+        "richtige antwort": "antwort 1"
+    },
+    {
+        "frage": "Welche Methode wird in Node.js verwendet, um eine Datei asynchron zu lesen?",
+        "antwort 1": "fs.read()",
+        "antwort 2": "fs.readfile()",
+        "antwort 3": "fs.readFile()",
+        "antwort 4": "fs.open()",
+        "richtige antwort": "antwort 3"
+    },
+    {
+        "frage": "Welche Methode wird in Node.js verwendet, um eine Datei asynchron zu schreiben?",
+        "antwort 1": "fs.write()",
+        "antwort 2": "fs.writeFile()",
+        "antwort 3": "fs.createWriteStream()",
+        "antwort 4": "fs.open()",
+        "richtige antwort": "antwort 2"
+    },
+    {
+        "frage": "Welches Modul wird in Node.js verwendet, um einen HTTP-Server zu erstellen?",
+        "antwort 1": "http",
+        "antwort 2": "server",
+        "antwort 3": "net",
+        "antwort 4": "web",
+        "richtige antwort": "antwort 1"
+    },
+    {
+        "frage": "Wie kann man in Node.js eine externe Bibliothek einbinden?",
+        "antwort 1": "Mit der require()-Funktion",
+        "antwort 2": "Mit der import()-Funktion",
+        "antwort 3": "Mit der include()-Funktion",
+        "antwort 4": "Mit der load()-Funktion",
+        "richtige antwort": "antwort 1"
+    },
+    {
+        "frage": "Wie kann man in Node.js eine Datei synchron lesen?",
+        "antwort 1": "fs.readFile()",
+        "antwort 2": "fs.readFileSync()",
+        "antwort 3": "fs.readfileSync()",
+        "antwort 4": "fs.open()",
+        "richtige antwort": "antwort 2"
+    },
+    {
+        "frage": "Wie kann man in Node.js eine Datei synchron schreiben?",
+        "antwort 1": "fs.writeFile()",
+        "antwort 2": "fs.writeFileSync()",
+        "antwort 3": "fs.createWriteStream()",
+        "antwort 4": "fs.open()",
+        "richtige antwort": "antwort 2"
+    },
+    {
+        "frage": "Wie kann man in Node.js eine Datei umbenennen?",
+        "antwort 1": "fs.rename()",
+        "antwort 2": "fs.renameFile()",
+        "antwort 3": "fs.move()",
+        "antwort 4": "fs.relocate()",
+        "richtige antwort": "antwort 1"
+    },
+    {
+        "frage": "Welche Methode wird in Node.js verwendet, um einen HTTP-Request zu senden?",
+        "antwort 1": "http.sendRequest()",
+        "antwort 2": "http.request()",
+        "antwort 3": "http.send()",
+        "antwort 4": "http.post()",
+        "richtige antwort": "antwort 2"
+    },
+    {
+        "frage": "Welches Node.js-Modul wird verwendet, um asynchrone Operationen zu verarbeiten?",
+        "antwort 1": "async",
+        "antwort 2": "event",
+        "antwort 3": "promise",
+        "antwort 4": "callback",
+        "richtige antwort": "antwort 4"
+    }
+]
